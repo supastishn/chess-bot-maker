aed9994a91b57674624ad1b2faac2248000ac151
supastishn <omareeto2012@hotmail.com>
2025-07-10 18:55:20 +0000

    build: remove chessAdapter.js and chess from optimizeDeps in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['chess'],
     esbuildOptions: {
       // For browsers, we need to polyfill these
       target: 'es2020',
