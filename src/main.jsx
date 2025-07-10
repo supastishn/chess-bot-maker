a06918819c18786e5f5ac77915372b1a195a73c3
supastishn <omareeto2012@hotmail.com>
2025-07-10 11:25:15 +0000

    feat: add eruda for in-app debugging

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import eruda from 'eruda'
 import './index.css'
 import App from './App.jsx'
+
+if (import.meta.env.DEV) {
+  eruda.init()
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
