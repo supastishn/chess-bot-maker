770ae2804caacc9191a2ee5a8eba2880a3eb4e78
supastishn <omareeto2012@hotmail.com>
2025-07-12 10:48:20 +0000

    build: switch to @vitejs/plugin-react-swc for React 19 compatibility

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'  // Updated React 19 compatible plugin
 
 export default defineConfig({
   plugins: [react()],
