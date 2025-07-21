c9869ad600398567e1b1f52d5e705a3ced4fb1b5
supastishn <omareeto2012@hotmail.com>
2025-07-21 06:49:37 +0000

    fix: centralize and register core block generators, remove duplicates

--- src/blockly/generators/game.js
+++ src/blockly/generators/game.js
@@ -1,46 +1,36 @@
 import * as Blockly from 'blockly/core';
-import { javascriptGenerator } from 'blockly/javascript';  // Add this import
+import { javascriptGenerator } from 'blockly/javascript';
 
-// Custom generators using javascriptGenerator directly
-javascriptGenerator['get_available_moves'] = function() {
-  return ['game.getAvailableMoves()', javascriptGenerator.ORDER_FUNCTION_CALL];
+// Centralized core block generators
+const coreGenerators = {
+  'get_available_moves': `game.getAvailableMoves()`,
+  'get_verbose_moves': `game.getVerboseMoves()`,
+  'get_turn': `game.getTurn()`,
+  'evaluate_material': `game.evaluateMaterial()`,
+  'get_book_moves': `game.getBookMoves()`,
+  'play_book_move': `game.playBookMove()`,
+  'get_game_phase': `game.getGamePhase()`,
+  'is_in_check': `game.isInCheck()`,
+  'is_checkmate': `game.isCheckmate()`,
+  'is_stalemate': `game.isStalemate()`,
+  'is_draw': `game.isDraw()`,
+  'is_game_over': `game.isGameOver()`,
+  'get_fen': `game.getFEN()`,
+  'get_move_count': `game.getMoveCount()`,
+  'get_position_score': `game.getPositionScore()`
 };
 
-javascriptGenerator['get_book_moves'] = function() {
-  return ['game.getBookMoves()', javascriptGenerator.ORDER_FUNCTION_CALL];
-};
+Object.entries(coreGenerators).forEach(([block, code]) => {
+  javascriptGenerator[block] = () => [code, javascriptGenerator.ORDER_FUNCTION_CALL];
+});
 
-javascriptGenerator['play_book_move'] = function() {
-  return ['game.playBookMove()', javascriptGenerator.ORDER_FUNCTION_CALL];
-};
-
-javascriptGenerator['get_turn'] = function() {
-  return ['game.getTurn()', javascriptGenerator.ORDER_FUNCTION_CALL];
-};
-
+// Statement block for returning a move
 javascriptGenerator['return_move'] = function(block) {
   const moveCode = javascriptGenerator.valueToCode(block, 'MOVE', javascriptGenerator.ORDER_NONE) || 'null';
   return `return ${moveCode};\n`;
 };
 
-javascriptGenerator['evaluate_material'] = function() {
-  return ['game.evaluateMaterial()', javascriptGenerator.ORDER_FUNCTION_CALL];
-};
-
-// --- Advanced Generators ---
-
-javascriptGenerator['is_in_check'] = function() {
-  return ['game.isInCheck()', javascriptGenerator.ORDER_FUNCTION_CALL];
-};
-
-javascriptGenerator['is_checkmate'] = function() {
-  return ['game.isCheckmate()', javascriptGenerator.ORDER_FUNCTION_CALL];
-};
-
-javascriptGenerator['get_position_score'] = function() {
-  return ['game.getPositionScore()', javascriptGenerator.ORDER_FUNCTION_CALL];
-};
-
+// Advanced generators
 javascriptGenerator['look_ahead'] = function(block) {
   const move = javascriptGenerator.valueToCode(block, 'MOVE', javascriptGenerator.ORDER_NONE) || "''";
   const depth = javascriptGenerator.valueToCode(block, 'DEPTH', javascriptGenerator.ORDER_ATOMIC) || 2;
@@ -48,38 +38,17 @@ javascriptGenerator['look_ahead'] = function(block) {
   return [`game.lookAhead(${move}, ${depth}).score`, javascriptGenerator.ORDER_FUNCTION_CALL];
 };
 
-javascriptGenerator['get_game_phase'] = function() {
-  return ['game.getGamePhase()', javascriptGenerator.ORDER_FUNCTION_CALL];
-};
-
 javascriptGenerator['get_threatened_squares'] = function(block) {
   const color = javascriptGenerator.valueToCode(block, 'COLOR', javascriptGenerator.ORDER_NONE) || "'w'";
   return [`game.getThreatenedSquares(${color})`, javascriptGenerator.ORDER_FUNCTION_CALL];
 };
 
-javascriptGenerator['get_verbose_moves'] = function() {
-  return ['game.getVerboseMoves()', javascriptGenerator.ORDER_FUNCTION_CALL];
-};
-javascriptGenerator['is_stalemate'] = function() {
-  return ['game.isStalemate()', javascriptGenerator.ORDER_FUNCTION_CALL];
-};
-javascriptGenerator['is_draw'] = function() {
-  return ['game.isDraw()', javascriptGenerator.ORDER_FUNCTION_CALL];
-};
-javascriptGenerator['is_game_over'] = function() {
-  return ['game.isGameOver()', javascriptGenerator.ORDER_FUNCTION_CALL];
-};
-javascriptGenerator['get_fen'] = function() {
-  return ['game.getFEN()', javascriptGenerator.ORDER_FUNCTION_CALL];
-};
-javascriptGenerator['get_move_count'] = function() {
-  return ['game.getMoveCount()', javascriptGenerator.ORDER_FUNCTION_CALL];
-};
 javascriptGenerator['is_attacked'] = function(block) {
   const square = javascriptGenerator.valueToCode(block, 'SQUARE', javascriptGenerator.ORDER_NONE) || "''";
   const color = javascriptGenerator.valueToCode(block, 'COLOR', javascriptGenerator.ORDER_NONE) || "'w'";
   return [`game.isAttacked(${square}, ${color})`, javascriptGenerator.ORDER_FUNCTION_CALL];
 };
+
 javascriptGenerator['prioritize_strategy'] = function(block) {
   const material = javascriptGenerator.valueToCode(block, 'MATERIAL', javascriptGenerator.ORDER_ATOMIC) || null;
   const development = javascriptGenerator.valueToCode(block, 'DEVELOPMENT', javascriptGenerator.ORDER_ATOMIC) || null;
@@ -93,19 +62,9 @@ javascriptGenerator['prioritize_strategy'] = function(block) {
   }`;
   return [`game.prioritizeStrategy(${weights})`, javascriptGenerator.ORDER_FUNCTION_CALL];
 };
-// --- Stockfish Generator ---
+
+// Stockfish generator
 javascriptGenerator['stockfish_move'] = function(block) {
-  const depth = javascriptGenerator.valueToCode(block, 'DEPTH', 
-    javascriptGenerator.ORDER_ATOMIC) || 15;
+  const depth = javascriptGenerator.valueToCode(block, 'DEPTH', javascriptGenerator.ORDER_ATOMIC) || 15;
   return [`await game.stockfish().getBestMove(game.getFEN(), ${depth})`, javascriptGenerator.ORDER_FUNCTION_CALL];
-};
-
-// Add these generators at the bottom of the file
-
-javascriptGenerator['get_verbose_moves'] = function() {
-  return ['game.getVerboseMoves()', javascriptGenerator.ORDER_FUNCTION_CALL];
-};
-
-javascriptGenerator['get_fen'] = function() {
-  return ['game.getFEN()', javascriptGenerator.ORDER_FUNCTION_CALL];
 };
